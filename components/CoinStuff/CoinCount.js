f519c8cc109a5596b375198c185099b3e2de6d68
Mike Collins <mike@192-168-1-139.tpgi.com.au>
2021-08-05 18:05:58 +1000

    coinIcon

--- components/CoinStuff/CoinCount.js
+++ components/CoinStuff/CoinCount.js
@@ -2,15 +2,18 @@ import React from "react";
 import CoinSvg from "./CoinSvg";
 import { Typography } from "@material-ui/core";
 import { withStyles } from "@material-ui/styles";
-
+import Coins from "../../public/images/coins.svg";
+import Image from "next/image";
 function CoinCount(props) {
   const { classes, coinCount, postId } = props;
 
   return coinCount > 0 ? (
     <div className={classes.container}>
-      <CoinSvg size={8} />
+      <div style={{ width: 30, height: 30 }}>
+        <Image src={Coins} alt="coins" />
+      </div>
       <div className={classes.text}>
-        <Typography variant="h6" color="primary">
+        <Typography variant="h6" color="primary" style={{ fontWeight: 900 }}>
           × {coinCount}
         </Typography>
       </div>
@@ -24,6 +27,8 @@ const styles = {
     flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
+    marginLeft: 5,
+    minWidth: 50,
     width: "auto",
     height: "auto",
   },
