290c838428a4b8aeb29556b2659f31ea4ab57a3d
Mike Collins <mike@192-168-1-139.tpgi.com.au>
2021-08-08 19:38:06 +1000

    metaTagForFullScreen

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,7 @@ export default class MyDocument extends Document {
         <Head>
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
+          <meta name="mobile-web-app-capable" content="yes"></meta>
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
